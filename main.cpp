31a72124c898b48b636ce847774befc2be03eab4
huuphuoc phamquang <huuphuoc.phamquang@gmail.com>
2025-12-14 13:48:07 +0700

    Sua ham main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
+#include <ctime>
+#include "Arena.h"
+
 using namespace std;
-#define H 20
-#define W 15
-char board[H][W] = {} ;
-char blocks[][4][4] = {
+
+Arena arena;
+
+char blocks[7][4][4] = {
         {{' ','I',' ',' '},
          {' ','I',' ',' '},
          {' ','I',' ',' '},
@@ -71,60 +74,46 @@ char blocks[][4][4] = {
          {'L','L','L',' '},
          {' ',' ',' ',' '}}
 };
-
-int x=4,y=0,b=1;
+int x=4, y=0, b=1;
 void gotoxy(int x, int y) {
     COORD c = {x, y};
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
+
 void boardDelBlock(){
-    for (int i = 0 ; i < 4 ; i++)
-        for (int j = 0 ; j < 4 ; j++)
-            if (blocks[b][i][j] != ' ' && y+j < H)
-                board[y+i][x+j] = ' ';
+    //for (int i = 0 ; i < 4 ; i++)
+    //    for (int j = 0 ; j < 4 ; j++)
+    //        if (blocks[b][i][j] != ' ' && y+j < H)
+    //            board[y+i][x+j] = ' ';
 }
 void block2Board(){
-    for (int i = 0 ; i < 4 ; i++)
-        for (int j = 0 ; j < 4 ; j++)
-            if (blocks[b][i][j] != ' ' )
-                board[y+i][x+j] = blocks[b][i][j];
+    arena.lockPiece(blocks[b], x, y);
 }
 void initBoard(){
-    for (int i = 0 ; i < H ; i++)
-        for (int j = 0 ; j < W ; j++)
-            if ((i==H-1) || (j==0) || (j == W-1)) board[i][j] = '#';
-            else board[i][j] = ' ';
+
 }
 void draw(){
     gotoxy(0,0);
     for (int i = 0 ; i < H ; i++, cout<<endl)
-        for (int j = 0 ; j < W ; j++)
-            cout<<board[i][j];
-}
-bool canMove(int dx, int dy){
-    for (int i = 0 ; i < 4 ; i++)
-        for (int j = 0 ; j < 4 ; j++)
-            if (blocks[b][i][j] != ' '){
-                int tx = x + j + dx;
-                int ty = y + i + dy;
-                if ( tx<1 || tx >= W-1 || ty >= H-1) return false;
-                if ( board[ty][tx] != ' ') return false;
+        for (int j = 0 ; j < W ; j++) {
+            // 1. Lấy dữ liệu nền từ Arena
+            char pixel = arena.getCell(i, j);
+
+            // 2. Nếu vị trí này trùng với khối đang rơi, vẽ đè lên
+            if (i >= y && i < y + 4 && j >= x && j < x + 4) {
+                if (blocks[b][i-y][j-x] != ' ') {
+                    pixel = blocks[b][i-y][j-x];
+                }
             }
-    return true;
+            cout << pixel;
+        }
+}
+
+bool canMove(int dx, int dy){
+    return arena.isValidPosition(blocks[b], x + dx, y + dy);
 }
 void removeLine(){
-    int j;
-    for (int i = H-2; i >0 ; i-- ){
-        for (j = 0; j < W-1 ; j++)
-            if (board[i][j] == ' ') break;
-        if (j == W-1){
-            for (int ii = i; ii >0 ; ii-- )
-                for (int j = 0; j < W-1 ; j++ ) board[ii][j] = board[ii-1][j];
-            i++;
-            draw();
-            _sleep(200);
-        }
-    }
+    arena.clearLines();
 }
 
 int main()
@@ -133,6 +122,7 @@ int main()
     b = rand() % 7;
     system("cls");
     initBoard();
+
     while (1){
         boardDelBlock();
         if (kbhit()){
@@ -146,11 +136,11 @@ int main()
         else {
             block2Board();
             removeLine();
-            x = 5; y = 0; b = rand() % 7;
+            x = 4; y = 0; b = rand() % 7;
         }
         block2Board();
         draw();
-        _sleep(200);
+        Sleep(200);
     }
     return 0;
 }
