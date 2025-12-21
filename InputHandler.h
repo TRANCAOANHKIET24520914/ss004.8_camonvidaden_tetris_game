f02e6fad37725280f58b1295bbfa9def0377befe
24521609-NguyenQuocThang <24521609@gm.uit.edu.vn>
2025-12-21 22:08:26 +0700

    Add files via upload

---
+++ InputHandler.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include<iostream>
+using namespace std;
+
+struct InputState {
+    bool left;
+    bool right;
+    bool down;
+    bool rotate;
+};
+
+class InputHandler {
+public:
+    InputHandler();
+
+    InputState getInput();     // Đọc input từ bàn phím
+};
